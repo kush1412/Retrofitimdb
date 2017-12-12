3d255a9677cca7ac9200bdf6eddcd699fb0e43ff
Kush <kykushy8@gmail.com>
2017-12-12 18:31:02 +0530

    RetrofitExample

---
+++ app/src/main/java/com/example/kush/retrofitimdb/ApiInterface.java
@@ -0,0 +1,19 @@
+package com.example.kush.retrofitimdb;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by Kush on 12-12-2017.
+ */
+
+public interface  ApiInterface {
+
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
